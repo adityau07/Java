7a0c1b1cea013b9e699d0eb27eb29618a0af9291
adityau07 <100702540+adityau07@users.noreply.github.com>
2025-07-06 22:36:22 +0530

    Add files via upload
    Check Sorted Array

---
+++ 03 Check Sorted/checkSorted.java
@@ -0,0 +1,23 @@
+class checkSorted
+{
+	public static void main(String[] args)
+	{
+		int[] arr = {1,2,3,4,5};
+		
+		System.out.println(isSorted(arr));
+	}
+	
+	static boolean isSorted(int[] arr)
+	{
+		int n = arr.length;
+		
+		for(int i=1; i<n; i++)
+		{
+			if(arr[i]<arr[i-1])
+			{
+				return false;
+			}
+		}
+		return true; 
+	}
+}
